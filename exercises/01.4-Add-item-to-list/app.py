13b93ba6dffaba3254af72f9245c057d3a90a8bd
Tibisfly <vielmatorotibisay@gmail.com>
2020-11-17 22:07:47 +0000

    I swear of God Wally..

--- exercises/01.4-Add-item-to-list/app.py
+++ exercises/01.4-Add-item-to-list/app.py
@@ -1,5 +1,13 @@
 #Remember import random function here:
+import random 
 
 my_list = [4,5,734,43,45]
 
-#The magic is here:
+index = list(range(0,10))
+
+
+
+for i in range(0,10):
+    my_random_list = random.randint(0, 10)
+    my_list.append(my_random_list)
+print(my_list)
--- exercises/01.5-loop-seventeen/app.py
+++ exercises/01.5-loop-seventeen/app.py
@@ -1,1 +1,3 @@
 # Your code here, have fun:
+for x in range(18):
+    print(x)
