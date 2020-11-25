9f695ce36906e86909eae39ec5c1fd573e58d1ad
Tibisfly <vielmatorotibisay@gmail.com>
2020-11-25 13:32:27 +0000

    falta

--- exercises/07-Do_while/app.py
+++ exercises/07-Do_while/app.py
@@ -1,11 +1,19 @@
-
 #Your code go here:
 arr = []
 x = 20
-while x >= 0:
+while x >= -1:
     if x % 5 is 0:
         print(str(x)+"!")
-    elif x is 20:
-        print("LIFTOFF")
-    print(x)
-    x -= 1
+        i=-1
+    elif x % 5 is not 0:
+        print(i)
+        x -= 1
+# i = 20
+# while i >-1:
+# if i % 5 is 0:
+# print(str(i) +"!")
+# i-=1
+# elif i % 5 is not 0:
+# print(i)
+# i-=1
+# print("LIFTOFF")
